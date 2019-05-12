e9759abcbf672fd89b52bf5fca81a2c7174bc6af
Rishi Panthee <rishipanthee@hotmail.com>
2019-05-12 16:46:23 -0500

    Help implemented

--- cli.js
+++ cli.js
@@ -1,7 +1,6 @@
 #! /usr/bin/env node
 var index = require("./index.js")
 
-
 if (process.argv[2] == "genList"){
     index.genList(res => {
         console.log(res)
@@ -12,12 +11,15 @@ if (process.argv[2] == "genList"){
         console.log(res)
         process.exit(0)
     })
-} else if (process.argv[2] == "send" || process.argv[2] == "start") {
+} else if (process.argv[2] == "send") {
     index.send(res => {
         console.log(res)
         process.exit(0)
     })
+} else if (process.argv[2] == "help") {
+    console.log("To see the amount to send to people, use `airdrop genList`. \nTo see if theres any nonsteem usernames in the poeple to send list, use `airdrop processList`.\nOnce you are ready to send, use `airdrop send`.")
+    process.exit(0)
 } else {
-    console.log("Error. Read readme")
+    console.log("Error. Read readme to understand how to use this, or use `airdrop help`.")
     process.exit(0)
 }
